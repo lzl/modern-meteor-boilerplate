43bff7f761714472bdfa7275507b4343870b301f
lizunlong <lizunlong@gmail.com>
2018-02-13 12:27:24 +0800

    Add cache at server render

--- imports/server/index.js
+++ imports/server/index.js
@@ -1,21 +1,43 @@
+import { Meteor } from 'meteor/meteor'
 import React from 'react'
 import { onPageLoad } from 'meteor/server-render'
-import { renderToNodeStream } from 'react-dom/server'
+import { renderToStaticMarkup } from 'react-dom/server'
 import { Helmet } from 'react-helmet'
+import LRU from 'lru-cache'
 
 import ServerRoutes from './routes'
 import './collections'
 import './fixtures'
 
+const ssrCache = LRU({
+  max: 500,
+  maxAge: 1000 * 60 * 60,
+})
+
+const getSSRCache = (url, context) => {
+  if (ssrCache.has(url.pathname)) {
+    return ssrCache.get(url.pathname)
+  } else {
+    // const htmlStream = renderToStaticNodeStream(<ServerRoutes url={url} context={context} />)
+    const htmlString = renderToStaticMarkup(<ServerRoutes url={url} context={context} />)
+    const helmet = Helmet.renderStatic()
+    const meta = helmet.meta.toString()
+    const title = helmet.title.toString()
+    const link = helmet.link.toString()
+    const newSSRCache = { html: htmlString, meta, title, link }
+    Meteor.defer(() => {
+      // ssrCache.set(url.pathname, { ...newSSRCache, html: htmlString })
+      ssrCache.set(url.pathname, newSSRCache)
+    })
+    return newSSRCache
+  }
+}
+
 onPageLoad(sink => {
   const context = {}
-
-  const html = renderToNodeStream(<ServerRoutes url={sink.request.url} context={context} />)
-
-  sink.renderIntoElementById('app', html)
-
-  const helmet = Helmet.renderStatic()
-  sink.appendToHead(helmet.meta.toString())
-  sink.appendToHead(helmet.title.toString())
-  sink.appendToHead(helmet.link.toString())
+  const cache = getSSRCache(sink.request.url, context)
+  sink.renderIntoElementById('app', cache.html)
+  sink.appendToHead(cache.meta)
+  sink.appendToHead(cache.title)
+  sink.appendToHead(cache.link)
 })
