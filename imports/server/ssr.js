23b19dbb62e8c51ef6314cc241d9f0728f5b37c5
lizunlong <lizunlong@gmail.com>
2018-02-13 15:13:58 +0800

    Adjust SSR

---
+++ imports/server/ssr.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor'
+import React from 'react'
+import { onPageLoad } from 'meteor/server-render'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { Helmet } from 'react-helmet'
+import LRU from 'lru-cache'
+import { ServerStyleSheet } from 'styled-components'
+
+import ServerRoutes from './routes'
+
+const ssrCache = LRU({
+  max: 500,
+  maxAge: 1000 * 60 * 60,
+})
+
+const getSSRCache = (url, context) => {
+  if (ssrCache.has(url.pathname)) {
+    return ssrCache.get(url.pathname)
+  } else {
+    const sheet = new ServerStyleSheet()
+    const jsx = sheet.collectStyles(<ServerRoutes url={url} context={context} />)
+    const html = renderToStaticMarkup(jsx)
+    const styleTags = sheet.getStyleTags()
+    const helmet = Helmet.renderStatic()
+    const meta = helmet.meta.toString()
+    const title = helmet.title.toString()
+    const link = helmet.link.toString()
+    const newSSRCache = { html, styleTags, meta, title, link }
+    Meteor.defer(() => {
+      ssrCache.set(url.pathname, newSSRCache)
+    })
+    return newSSRCache
+  }
+}
+
+onPageLoad(sink => {
+  const context = {}
+  const cache = getSSRCache(sink.request.url, context)
+  sink.renderIntoElementById('app', cache.html)
+  sink.appendToHead(cache.meta)
+  sink.appendToHead(cache.title)
+  sink.appendToHead(cache.link)
+  sink.appendToHead(cache.styleTags)
+})
